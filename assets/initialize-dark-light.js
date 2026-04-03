67f0de7cfdfa65e0c74006ecc393add400d7c163
ryanbethel <ryanbethel@users.noreply.github.com>
2026-04-03 02:52:07 +0000

    deploy: 1707705d9e57b9aceb21f945a8edcb286a608e6f

--- assets/initialize-dark-light.js
+++
@@ -1,9 +0,0 @@
-const darkLightTheme = window.localStorage.getItem('dark-light-theme');
-if (darkLightTheme === 'dark') {
-  document.documentElement.classList.add('dark-mode');
-  document.documentElement.classList.remove('light-mode')
-}
-if (darkLightTheme === 'light') {
-  document.documentElement.classList.add('light-mode')
-  document.documentElement.classList.remove('dark-mode');
-} 
